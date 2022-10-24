83335cf504661e4038a88dbf3b1cf7273c61cf84
LuthandoMtengwane <luthandolindokuhle17@gmail.com>
2022-10-24 23:20:49 +0200

    second commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Image1 from "../images/1.png";
+import Image2 from "../images/2.png";
 import "../styles/components/work.scss";
 
 const Work = () => {
   return (
     <div className="work">
       <div className="work-container">
-        <div className="work-grid-container"></div>
+        <div className="work-grid-container">
+          <div className="work-grid-image1">
+            <img src={Image1} alt="first project" />
+          </div>
+          <div className="work-grid-image2">
+            <img src={Image2} alt="second-project" />
+          </div>
+        </div>
       </div>
     </div>
   );
